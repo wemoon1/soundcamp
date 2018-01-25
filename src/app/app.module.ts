ba5354a5411665bc3cb23d74242808183e939d1a
wemoon1 <wemoon@ucsc.edu>
2018-01-24 23:40:10 -0800

    included home and about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+/* Modules */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+/* Components */
 import { AppComponent } from './app.component';
-
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
