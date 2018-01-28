08eba3643142be141833cfd5eb54de218bc86b45
wemoon1 <wemoon@ucsc.edu>
2018-01-27 23:33:32 -0800

    imported components and updated routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,36 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { EventListComponent } from './event-list/event-list.component';
+import { EventCardComponent } from './event-card/event-card.component';
+import { EventDetailComponent } from './event-detail/event-detail.component';
+import { SearchListComponent } from './search-list/search-list.component';
+import { SearchFormComponent } from './search-form/search-form.component';
+import { SearchPageComponent } from './search-page/search-page.component';
 
 /* Routes (move to separate file?) */
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'search', component: SearchPageComponent,
+    children: [
+      { path: ':id', component: SearchListComponent }
+    ]
+  },
+  { path: 'event/:id', component: EventDetailComponent },
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    AboutComponent
+    AboutComponent,
+    EventListComponent,
+    EventCardComponent,
+    EventDetailComponent,
+    SearchListComponent,
+    SearchFormComponent,
+    SearchPageComponent
   ],
   imports: [
     BrowserModule,
