65444ee19005569f70a8d3bb402093e2bd9adea3
dmclella1644 <31871226+dmclella1644@users.noreply.github.com>
2018-02-05 11:49:44 -0800

    Add files via upload
    initial commit (missing .css files)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
+/* Modules */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-
+/* Components */
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { ArtistCardComponent } from './artist-card/artist-card.component';
+import { ArtistListComponent } from './artist-list/artist-list.component';
+import { ArtistDetailComponent } from './artist-detail/artist-detail.component';
 
+/* Routes (move to separate file?) */
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'artist/:id', component: ArtistDetailComponent },
+  { path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    LoginComponent,
+    ArtistCardComponent,
+    ArtistListComponent,
+    ArtistDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
