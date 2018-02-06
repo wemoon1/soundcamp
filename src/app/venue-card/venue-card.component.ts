b0a9dc0197a1a5cf054659a20ad39928ca62a0fe
wemoon1 <wemoon@ucsc.edu>
2018-02-05 20:35:57 -0800

    remove direct api calls

--- src/app/venue-card/venue-card.component.ts
+++ src/app/venue-card/venue-card.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { HttpClient } from  '@angular/common/http';
-import {Observable} from "rxjs/Observable";
-import {Http,Response} from "@angular/http";
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -11,34 +8,19 @@ import 'rxjs/add/operator/map';
 })
 export class VenueCardComponent implements OnInit {
 
-  id:any;
-  country:any;
-  state:any;
-  name:any;
-  street:any;
-   @Input() description:any;
-  website:any;
-  zip:any;
-  phone:any;
+    id:any;
+    country:any;
+    state:any;
+    name:any;
+    street:any;
+    @Input() description:any;
+    website:any;
+    zip:any;
+    phone:any;
 
+    constructor() { }
 
-    constructor(private http: HttpClient) {
+    ngOnInit() {
 
     }
-    ngOnInit():void {
-      this.http.get('http://api.songkick.com/api/3.0/venues/222.json?apikey=io09K9l3ebJxmxe2')
-                      .subscribe((data : any)=> {
-                        this.id = data.resultsPage.results.venue.id;
-                        this.country = data.resultsPage.results.venue.city.country.displayName;
-                        this.state = data.resultsPage.results.venue.city.state.displayName;
-                        this.name = data.resultsPage.results.venue.displayName;
-                        this.description = data.resultsPage.results.venue.description;
-                        this.street = data.resultsPage.results.venue.street;
-                        this.zip = data.resultsPage.results.venue.zip;
-                        this.phone = data.resultsPage.results.venue.phone;
-                        this.website = data.resultsPage.results.venue.website;
-                        console.log(data);
-
-                      });
-                    }
   }
