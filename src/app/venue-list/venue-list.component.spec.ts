dad1cf94b784673063c04d01db1d7cd30b2ad3cf
wemoon1 <wemoon@ucsc.edu>
2018-02-05 20:36:11 -0800

    import NO_ERRORS_SCHEMA to test spec

--- src/app/venue-list/venue-list.component.spec.ts
+++ src/app/venue-list/venue-list.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { VenueListComponent } from './venue-list.component';
 
@@ -8,7 +9,8 @@ describe('VenueListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ VenueListComponent ]
+      declarations: [ VenueListComponent ],
+      schemas: [ NO_ERRORS_SCHEMA ]
     })
     .compileComponents();
   }));
