0b0e43a30bbbad4ea5b53b246af9f8e4b7c5ea98
wemoon1 <wemoon@ucsc.edu>
2018-02-14 21:32:35 -0800

    finalize search form

--- src/app/search/search-form/search-form.component.ts
+++ src/app/search/search-form/search-form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-form',
@@ -6,10 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
+  searchForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router
+  ) {
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this.searchForm = this.formBuilder.group({
+      search: ['', Validators.required],
+    });
+  }
+
+  search() {
+    const searchQuery = this.searchForm.value.search;
+    if (searchQuery) {
+      this.router.navigate(['search', searchQuery]);
+    } else {
+      console.log('Empty value, please enter something');
+    }
   }
 
 }
