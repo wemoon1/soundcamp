296da23b8c76fcd513b473ebe04afd8f540179bd
wemoon1 <wemoon@ucsc.edu>
2018-02-15 10:59:09 -0800

    finalize event card

--- src/app/event/event-card/event-card.component.ts
+++ src/app/event/event-card/event-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-event-card',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-card.component.css']
 })
 export class EventCardComponent implements OnInit {
+  @Input() eventName;
+  @Input() id;
 
   constructor() { }
 
