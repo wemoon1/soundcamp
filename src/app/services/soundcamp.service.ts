8cf5be32ddd7b7018d7c600ed62d287e5576f1f5
wemoon1 <wemoon@ucsc.edu>
2018-02-15 17:15:21 -0800

    add urlBuilder function

--- src/app/services/soundcamp.service.ts
+++ src/app/services/soundcamp.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class SoundcampService {
+  private BASE_URL = 'https://desolate-harbor-18722.herokuapp.com/api';
 
   constructor() { }
 
+  private urlBuilder(resourceUrl: string, params?: Array<string>) {
+    let url = `${this.BASE_URL}${resourceUrl}`;
+    if (params) {
+      url += `${params.join('/')}`;
+    }
+    return url;
+  }
 }
