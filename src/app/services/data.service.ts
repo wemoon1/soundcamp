d622b97833d0b1ad5ab46e1c494a314a1f0c4c1f
wemoon1 <wemoon@ucsc.edu>
2018-02-15 17:11:18 -0800

    add get/set single event function

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,7 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataService {
+  singleEvent: any;
 
   constructor() { }
 
+  setSingleEvent(response: any) {
+    this.singleEvent = response;
+  }
+  getSingleEvent(): any {
+    return this.singleEvent;
+  }
 }
