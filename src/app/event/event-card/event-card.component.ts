497c64ce0944535b787614c76c3e79689168157c
wemoon1 <wemoon@ucsc.edu>
2018-02-15 17:07:23 -0800

    finalize event card

--- src/app/event/event-card/event-card.component.ts
+++ src/app/event/event-card/event-card.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit, Input } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
 @Component({
   selector: 'app-event-card',
   templateUrl: './event-card.component.html',
   styleUrls: ['./event-card.component.css']
 })
 export class EventCardComponent implements OnInit {
-  @Input() eventName;
-  @Input() id;
-
-  constructor() { }
+  @Input() event;
+  eventName;
+  id;
+  constructor(private router: Router, private data: DataService) { }
 
   ngOnInit() {
+    if (this.event) {
+      this.eventName = this.event.displayName;
+      this.id = this.event.id;
+    }
+  }
+
+  selectEvent() {
+    this.data.setSelectedEvent(this.event);
+    this.router.navigate(['event', this.id]);
   }
 
 }
