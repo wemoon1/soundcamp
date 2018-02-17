2c1748c2b77785d699cd36e9e76962f67775f79b
wemoon1 <wemoon@ucsc.edu>
2018-02-17 10:55:49 -0800

    add venue related functions; revised urlBuilder

--- src/app/services/soundcamp.service.ts
+++ src/app/services/soundcamp.service.ts
@@ -1,17 +1,45 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class SoundcampService {
-  private BASE_URL = 'https://desolate-harbor-18722.herokuapp.com/api';
+  private BASE_URL = 'https://desolate-harbor-18722.herokuapp.com/api/';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
-  private urlBuilder(resourceUrl: string, params?: Array<string>) {
-    let url = `${this.BASE_URL}${resourceUrl}`;
-    if (params) {
-      url += `${params.join('/')}`;
-    }
-    return url;
+  /*
+   * urlBuilder
+   * useage:
+   * resourceUrl = venues/search/catalyst/1
+   *
+   * url output should look something like:
+   * https://desolate-harbor-18722.herokuapp.com/api/venue/search/catalyst/1
+   */
+  private urlBuilder(resourceUrl: string) {
+    return `${this.BASE_URL}${resourceUrl}`;
+  }
+
+  /*
+   * getVenues
+   * useage:
+   * query = 'catalyst'
+   * pageNumber = 1
+   *
+   * returns Observable which you can call .subscribe() on it:
+   * SoundcampService.getVenues(query, pageNumber).subscribe(data => console.log(data));
+   */
+  getVenues(query: string, pageNumber: number): Observable<object> {
+    const resourceUrl = 'venues/search/' + query + '/' + pageNumber;
+    const requestUrl = this.urlBuilder(resourceUrl);
+
+    return this.http.get(requestUrl);
+  }
+
+  getVenueEvents(venueId: string, pageNumber: number): Observable<object> {
+    const resourceUrl = 'venues/' + venueId + '/events/' + pageNumber;
+    const requestUrl = this.urlBuilder(resourceUrl);
+
+    return this.http.get(requestUrl);
   }
 }
