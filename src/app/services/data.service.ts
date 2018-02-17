d44148ab8af053f01d7e22a8375733435254db04
wemoon1 <wemoon@ucsc.edu>
2018-02-17 10:56:12 -0800

    add comment

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -2,6 +2,14 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class DataService {
+  /* stores a single element from the array "event", e.g
+    "resultsPage":{
+      "results":{
+        "event":[
+          { ... }, <-- store this
+          { ... }
+        ]}}
+  */
   singleEvent: any;
 
   constructor() { }
