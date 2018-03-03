9e2d7fa3fad7834682fc70247154bf6b33c01d42
wemoon1 <wemoon@ucsc.edu>
2018-03-02 17:27:21 -0800

    remove comments and add onFollow method

--- src/app/artist/artist-card/artist-card.component.ts
+++ src/app/artist/artist-card/artist-card.component.ts
@@ -7,14 +7,6 @@ import { Router } from '@angular/router';
   styleUrls: ['./artist-card.component.css']
 })
 export class ArtistCardComponent implements OnInit {
-  // todo:
-  // 1) import DataService and Router and inject it
-  // 2) find and store the information from input variable 'artist' such as names and artist id
-  // 3.1) create a function onSelected() which calls DataService and stores the input variable 'artist' to the service by calling saveArtist()
-  // 3.2) call router and navigate to artist detail page with argument artist id
-  // 4) in the template display the artist name and show if the artist is on tour or not by checking 'onTourUntil'
-  // 5) when the artist is clicked by user, call onSelected()
-  // see event-card.component.ts for help
   @Input() artist;
   artistId;
   artistName;
@@ -37,5 +29,9 @@ export class ArtistCardComponent implements OnInit {
   onSelected() {
     this.data.saveArtist(this.artist);
     this.router.navigate(['artist', this.artistId]);
+  }
+
+  onFollow() {
+    console.log('following...');
   }
 }
