c8431d07a5e8b000dbff6533807171b0a60b8f45
dmclella1644 <31871226+dmclella1644@users.noreply.github.com>
2018-03-02 17:03:59 -0800

    added saveVenue and loadVenue functions

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -11,6 +11,7 @@ export class DataService {
         ]}}
   */
   event: any;
+  venue: any;
 
   constructor() { }
 
@@ -20,5 +21,13 @@ export class DataService {
 
   loadEvent(): any {
     return this.event;
+  }
+  
+  saveVenue(response:any) {
+    this.venue = response;
+  }
+  
+  loadVenue(): any {
+    return this.venue;
   }
 }
