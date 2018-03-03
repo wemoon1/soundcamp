73711d37873b875dc508bbf91e0aaad876242247
wemoon1 <wemoon@ucsc.edu>
2018-03-02 17:06:40 -0800

    add show more button and functionality to upcoming events

--- src/app/artist/artist-detail/artist-detail.component.ts
+++ src/app/artist/artist-detail/artist-detail.component.ts
@@ -15,8 +15,14 @@ export class ArtistDetailComponent implements OnInit {
   // 4) in the template, call *ngFor on 'event' and pass each element to event-list component
   // see event-card.component.ts for help
   artist: any;
-  upcomingEvents: any;
+  upcomingEvents = [];
+  totalUpcomingEvents = 0;
+  currentPageUpcoming = 1;
+  upcomingEventsShown;
+  perPage = 5;
+
   onTour = false;
+
   constructor(private data: DataService, private service: SoundcampService) { }
 
   ngOnInit() {
@@ -30,10 +36,34 @@ export class ArtistDetailComponent implements OnInit {
   }
 
   getUpcomingEvents() {
-    this.service.getArtistEvents(this.artist.id, 1).subscribe(data => {
-      this.upcomingEvents = data;
-      this.upcomingEvents = this.upcomingEvents.resultsPage.results.event;
+    this.service.getArtistEvents(this.artist.id, this.currentPageUpcoming).subscribe(data => {
+      this.parseUpcomingEvents(data);
+      // this.upcomingEvents = this.upcomingEvents.resultsPage.results.event;
     });
+  }
+
+  parseUpcomingEvents(events: any) {
+    if (this.currentPageUpcoming < 2) {
+      this.totalUpcomingEvents = events.resultsPage.totalEntries;
+    }
+    this.upcomingEventsShown = this.currentPageUpcoming * this.perPage;
+    if (this.totalUpcomingEvents) {
+      for (const event of events.resultsPage.results.event) {
+        this.upcomingEvents.push(event);
+      }
+    }
+  }
+
+  showMoreUpcoming() {
+    this.currentPageUpcoming += 1;
+    this.getUpcomingEvents();
+  }
+
+  clearResults() {
+    this.upcomingEvents = [];
+    this.totalUpcomingEvents = 0;
+    this.currentPageUpcoming = 1;
+    this.upcomingEventsShown = 0;
   }
 
 }
