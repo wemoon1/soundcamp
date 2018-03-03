615adba61a99dc9ad664343330a6c1d26687874f
dmclella1644 <31871226+dmclella1644@users.noreply.github.com>
2018-03-02 17:02:53 -0800

    added code to .component.ts parts for venue

--- src/app/venue/venue-card/venue-card.component.ts
+++ src/app/venue/venue-card/venue-card.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../../services/data.service';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -8,14 +10,11 @@ import 'rxjs/add/operator/map';
 })
 export class VenueCardComponent implements OnInit {
   // todo:
-  // 1) import DataService and Router and inject it
-  // 2) find and store the information from input variable 'venue' such as names and artist id
-  // 3.1) create a function onSelected() which calls DataService and stores the input variable 'venue' to the data service by calling saveVenue()
-  // 3.2) in the same function call router and navigate to venue detail page with venue id as argument
   // 4) in the template show venue name, location, and other useful information but not too much
   // 5) when the venue is clicked by user, call onSelected()
   // see event-card.component.ts for help
   @Input() venue;
+  venueName:any;
   id:any;
   country:any;
   state:any;
@@ -24,10 +23,18 @@ export class VenueCardComponent implements OnInit {
   website:any;
   zip:any;
   phone:any;
-
-    constructor() { }
-
-    ngOnInit() {
-
+  constructor(private router: Router, private data: DataService) { }
+  
+  ngOnInit() {
+    if (this.venue) {
+      this.venueName = this.venue.displayName;
+      this.id = this.venue.id;
+	  console.log("id: " + this.id + " venueName: " + this.venueName);
     }
   }
+
+  onSelected() {
+    this.data.saveVenue(this.venue);
+    this.router.navigate(['venue', this.id]);
+  }
+}
--- src/app/venue/venue-list/venue-list.component.ts
+++ src/app/venue/venue-list/venue-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from 'rxjs/Observable';
-import {Http, Response} from '@angular/http';
+import { Http, Response } from '@angular/http';
+import { SoundcampService } from '../../services/soundcamp.service';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 
@@ -13,15 +14,18 @@ import 'rxjs/add/operator/map';
 })
 export class VenueListComponent implements OnInit {
     // todo:
-  // 1) import Soundcamp service and inject it
-  // 2) call getVenues() from SoundcampService and pass the argument searchQuery into the function and subscribe to it
-  // 2.5) inside .subscribe(), find and save the array object "venue" to a variable e.g venueResult
   // 3) in the template, loop through venueResult and pass each element to child component venue-card
-
+  venueResult:any;
   @Input() searchQuery;
-    constructor() { }
+  
+  constructor(private service: SoundcampService){ }
 
-    ngOnInit() {
-
+  ngOnInit() {
+    if (this.searchQuery) {
+	  this.service.getVenues(this.searchQuery, 1).subscribe(data => {
+	    console.log("data " + data);
+	    this.venueResult = data;
+	  });
+	}
   }
-  }
+}
