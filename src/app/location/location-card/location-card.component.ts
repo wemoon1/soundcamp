aea6e0f5bd18cd558c082fbc7b1e7922dfde6f14
wemoon1 <wemoon@ucsc.edu>
2018-03-05 17:16:31 -0800

    fixes to location list and card component

--- src/app/location/location-card/location-card.component.ts
+++ src/app/location/location-card/location-card.component.ts
@@ -7,42 +7,27 @@ import {ActivatedRoute} from '@angular/router';
   selector: 'app-location-card',
   templateUrl: './location-card.component.html',
   styleUrls: ['./location-card.component.css'],
-  providers: [SoundcampService,DataService]
 })
 export class LocationCardComponent implements OnInit {
-  // todo:
-  // 1) import DataService and Router and inject it
-  // 2) create a variable for storing: city, country, state, and metroArea id
-  // 3) find and store the information from input variable location to said variables
-  // 4.1) create a function onSelected() which calls DataService and stores the
-  // input variable 'location' to the service by calling saveLocation()
-  // 4.2) call router and navigate to location with argument metroArea id
-  // 5) in the template display the city name, state, and country
-  // 6) when the location is clicked by user, call onSelected()
-  // see event-card.component.ts for help
-  @Input() searchQuery;
-  searchRes = [];
-  searchEvent=[];
+  @Input() location;
 
-  constructor(private service: SoundcampService,private route: ActivatedRoute,
+  constructor(private service: SoundcampService, private route: ActivatedRoute,
     private dataservice: DataService) { }
 
   ngOnInit() {
 
-this.getData();
+// this.getData();
 
 
 
   }
-  getData(){
-    this.service.getLocation(this.searchQuery, 1).subscribe((data:any) =>{
-      this.searchRes=data.resultsPage.results.location;
-      console.log(this.searchQuery);
-      console.log('this is data',this.searchRes);
-
-
-      })
-    }
+  // getData(){
+  //   this.service.getLocation(this.searchQuery, 1).subscribe((data:any) =>{
+  //     this.searchRes=data.resultsPage.results.location;
+  //     console.log(this.searchQuery);
+  //     console.log('this is data',this.searchRes);
+  //     })
+  //   }
 
 
 }
--- src/app/location/location-list/location-list.component.ts
+++ src/app/location/location-list/location-list.component.ts
@@ -1,8 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, OnChanges } from '@angular/core';
 import { SoundcampService } from '../../services/soundcamp.service';
 import { DataService } from '../../services/data.service';
-
-//import {Location} from '../../Location';
 
 @Component({
   selector: 'app-location-list',
@@ -10,24 +8,21 @@ import { DataService } from '../../services/data.service';
   styleUrls: ['./location-list.component.css'],
 
 })
-export class LocationListComponent implements OnInit {
-
-  // todo:
-  // 1) import Soundcamp service and inject it
-  // 2) call getLocations() and pass the argument searchQuery into the function and subscribe to it
-  // 2.5) inside .subscribe(), find and save the array object "location" to a variable e.g locationResult
-  // 3) in the template, loop through locationResult and pass each element to child component location-card
+export class LocationListComponent implements OnInit, OnChanges {
   @Input() searchQuery;
+  locationResults;
 
-  constructor(private service: SoundcampService,private dataservice: DataService) { }
+  constructor(private service: SoundcampService, private dataservice: DataService) { }
 
-ngOnInit() {
-
-
+  ngOnInit() {
   }
 
+  ngOnChanges() {
+    this.searchArtists();
+  }
 
-
-
-
+  searchArtists() {
+    this.service.getArtists(this.searchQuery, 1).subscribe(
+      (data: any) => this.locationResults = data);
+  }
 }
