4037135ca2134421544e40b547704fc5e535d181
wemoon1 <wemoon@ucsc.edu>
2018-03-15 16:00:43 -0700

    generate notification component

---
+++ src/app/notification/notification.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent implements OnInit {
+  notificationItems;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
