545a2897516d355518d67b6aab1e118bc1e70337
wemoon1 <wemoon@ucsc.edu>
2018-03-15 16:01:01 -0700

    add getArtistPastEvents function

--- src/app/services/soundcamp.service.ts
+++ src/app/services/soundcamp.service.ts
@@ -80,6 +80,13 @@ export class SoundcampService {
     return this.http.get(requestUrl);
   }
 
+  getArtistPastEvents(artistId: string, pageNumber: number): Observable<object> {
+    const resourceUrl = 'artists/' + artistId + '/gigography/' + pageNumber;
+    const requestUrl = this.urlBuilder(resourceUrl);
+
+    return this.http.get(requestUrl);
+  }
+
   getNearestEvents(position: Position, pageNumber: number): Observable<object> {
     const resourceUrl = 'locations/' + position.coords.latitude + '/' + position.coords.longitude + '/events/' + pageNumber;
     const requestUrl = this.urlBuilder(resourceUrl);
