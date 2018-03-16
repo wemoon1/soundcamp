97a4282720db0920fe43fd514351189e9dd69977
wemoon1 <wemoon@ucsc.edu>
2018-03-15 23:24:23 -0700

    minor changes to location detail

--- src/app/location/location-detail/location-detail.component.ts
+++ src/app/location/location-detail/location-detail.component.ts
@@ -1,21 +1,29 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SoundcampService } from '../../services/soundcamp.service';
 import { DataService } from '../../services/data.service';
-import {ActivatedRoute} from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+
 @Component({
   selector: 'app-location-detail',
   templateUrl: './location-detail.component.html',
   styleUrls: ['./location-detail.component.css'],
 })
 export class LocationDetailComponent implements OnInit {
-  @Input() location;
+  location: any;
   searchEvent = [];
   pagenumber = 1;
+
   constructor(private service: SoundcampService, private route: ActivatedRoute,
-    private dataservice: DataService) { }
+    private dataservice: DataService,
+  private router: Router) { }
 
   ngOnInit() {
-    this.eventdata();
+    this.location = this.dataservice.loadLocation();
+    if(this.location) {
+      this.eventdata();
+    } else {
+      this.router.navigate(['']);
+    }
   }
 
   eventdata(): void {
