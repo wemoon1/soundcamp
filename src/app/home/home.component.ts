6834d0c0f4ade3fc92aa1aaaa83317f723813afe
wemoon1 <wemoon@ucsc.edu>
2018-03-15 18:17:42 -0700

    add notify event

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { SoundcampService } from '../services/soundcamp.service';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { DataService } from '../services/data.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -11,9 +15,13 @@ export class HomeComponent implements OnInit {
   loading = true;
   events: any;
 
-  constructor(private soundcamp: SoundcampService) { }
+  constructor(private soundcamp: SoundcampService,
+              private afAuth: AngularFireAuth,
+              private afs: AngularFirestore,
+              private data: DataService) { }
 
   ngOnInit() {
+    this.notifyEvents();
     // if (window.navigator.geolocation) {
     //   window.navigator.geolocation.getCurrentPosition(
     //     (position) => {
@@ -26,6 +34,42 @@ export class HomeComponent implements OnInit {
     //     }
     //   );
     // }
+  }
+
+  notifyEvents() {
+    // sample id: mock@gmail.com | mock123Q
+    const followingArtists = {};
+    this.afAuth.auth.onAuthStateChanged((user) => {
+      if (user) {
+        this.afs.collection('users')
+        .doc(user.uid)
+        .collection('following-artists').ref.get().then(function(querySnapshot) {
+          querySnapshot.forEach(function(doc) {
+            followingArtists[doc.id] = doc.data();
+          });
+        }).then(() => {
+          this.findArtistsOnTour(followingArtists);
+        });
+      } else {
+        console.log('user not logged in');
+      }
+    });
+  }
+
+  findArtistsOnTour(artists: any) {
+    const artistsOnTour = [];
+    for (const key in artists) {
+      if (artists.hasOwnProperty(key)) {
+        const artistId = artists[key].id;
+        artistsOnTour.push(key)
+        // this.soundcamp.getArtistEvents(artistId, 1).subscribe((res: any) => {
+        //   if (res.resultsPage.totalEntries > 0) {
+        //     artistsOnTour.push(key);
+        //   }
+        // });
+      }
+    }
+    this.data.changeMsg(artists);
   }
 
 }
