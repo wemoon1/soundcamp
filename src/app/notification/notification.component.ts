14f1ef50c25438a9584bf21e0419470c817bd95a
wemoon1 <wemoon@ucsc.edu>
2018-03-15 18:17:14 -0700

    changes to notification drop down

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { DataService } from '../services/data.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-notification',
@@ -7,10 +9,29 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class NotificationComponent implements OnInit {
   notificationItems;
-
-  constructor() { }
+  displayName = [];
+  constructor(private data: DataService, private router: Router) { }
 
   ngOnInit() {
+    this.data.currentMsg.subscribe(newMsg => {
+      this.displayName = [];
+      this.notificationItems = newMsg;
+      for (const key in newMsg) {
+        if (newMsg.hasOwnProperty(key)) {
+          this.displayName.push(key);
+        }
+      }
+    });
   }
 
+  onSelected(artistName) {
+    const artistId = this.notificationItems[artistName].id;
+    const artistPayload = {
+      'displayName' : artistName,
+      'id': artistId,
+      'onTourUntil': 'yyyy-mm-dd'
+    };
+    this.data.saveArtist(artistPayload);
+    this.router.navigate(['artist', artistId]);
+  }
 }
