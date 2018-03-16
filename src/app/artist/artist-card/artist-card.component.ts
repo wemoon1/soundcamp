c12c87ec7e84b9c9ab00b20cf192618522c03a21
dmclella1644 <31871226+dmclella1644@users.noreply.github.com>
2018-03-15 18:52:02 -0700

    added unfollow method

--- src/app/artist/artist-card/artist-card.component.ts
+++ src/app/artist/artist-card/artist-card.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Router } from '@angular/router';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable'
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AuthService } from '../../core/auth.service';
 @Component({
   selector: 'app-artist-card',
   templateUrl: './artist-card.component.html',
@@ -10,11 +14,24 @@ export class ArtistCardComponent implements OnInit {
   @Input() artist;
   artistId;
   artistName;
+  user;
+  followingArtists: Object[];
 
-  constructor(private data: DataService, private router: Router) { }
+  constructor(private data: DataService,
+			  private router: Router,
+			  private afAuth: AngularFireAuth,
+              private afs: AngularFirestore,
+			  public Auth: AuthService) { }
 
   ngOnInit() {
     this.parseArtistData();
+	const artistsFollowing = [];
+	this.afs.collection('users').doc(this.afAuth.auth.currentUser.uid).collection('following-artists').ref.get().then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
+	    artistsFollowing[doc.data().id] = doc.id;
+      });
+    });
+	this.followingArtists = artistsFollowing;
   }
 
   parseArtistData() {
@@ -32,6 +49,33 @@ export class ArtistCardComponent implements OnInit {
   }
 
   onFollow() {
-    console.log('following...');
+    this.user = this.afAuth.auth.currentUser
+    if (this.user) {
+      console.log("currently signed in user:")
+      console.log(this.user)
+      console.log(this.user.displayName)
+      console.log(this)
+        if (this.artist) {
+          this.afs.collection("users").doc(this.user.uid).collection("following-artists")
+          .doc(this.artistName).set({id: this.artistId},{merge: true})
+          console.log('following...');
+          console.log(this.artistName);
+		  this.followingArtists[this.artistId] = this.artistName;
+		  console.log(this.followingArtists);
+        }
+        else {
+          console.log('empty artist')
+        }
+    }
+    else {
+      alert("not logged in!")
+    }
+  }
+  
+  unFollow() {
+    this.user = this.afAuth.auth.currentUser
+	this.afs.collection("users").doc(this.user.uid).collection("following-artists")
+    .doc(String(this.followingArtists[this.artistId])).delete();
+	this.followingArtists.splice(this.artistId)
   }
 }
