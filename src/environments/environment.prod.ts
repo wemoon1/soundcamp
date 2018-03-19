7c257fec9a9795b656df62bbd4a59d43f232ac2a
wemoon1 <wemoon@ucsc.edu>
2018-03-18 17:42:10 -0700

    move firebase info to environment.prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDZFUXOMRkWTiQRbcsIlrzoiPCyTThdJKY",
+    authDomain: "soundcamp-cmps183.firebaseapp.com",
+    databaseURL: "https://soundcamp-cmps183.firebaseio.com",
+    projectId: "soundcamp-cmps183",
+    storageBucket: "soundcamp-cmps183.appspot.com",
+    messagingSenderId: "340392220045"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,4 @@
 
 export const environment = {
   production: false,
-
-  firebase: {
-    apiKey: "AIzaSyDZFUXOMRkWTiQRbcsIlrzoiPCyTThdJKY",
-    authDomain: "soundcamp-cmps183.firebaseapp.com",
-    databaseURL: "https://soundcamp-cmps183.firebaseio.com",
-    projectId: "soundcamp-cmps183",
-    storageBucket: "soundcamp-cmps183.appspot.com",
-    messagingSenderId: "340392220045"
-  }
 };
